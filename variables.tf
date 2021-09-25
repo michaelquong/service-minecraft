30228465a52af23d5854f2368781eb21b30fdb3e
root <root@DESKTOP-9UIUKFS.localdomain>
2021-09-24 20:13:00 -0700

    fmt corrections`

--- variables.tf
+++ variables.tf
@@ -6,12 +6,12 @@ variable "region" {
 
 variable "admin_ip_addresses" {
   description = "IP addresses authorized to access Instance"
-  type = list(string)
-  default = ["0.0.0.0/0"]
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
 }
 
 variable "friend_list" {
   description = "IP addresses allowed to join to server"
-  type = list(string)
-  default = ["0.0.0.0/0"]
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
 }
